fb7db0a50614970f1e52ea2d4695b1af74c5a7f0
anthony.cruz.c <anthony.cruz.c@89f4cc1e-9d26-0410-84c1-4d85e061beb3>
2007-01-16 23:01:59 +0000

    version con Smarty en orden, modificar el path del archivo admin/config/general_inc.php con el path de los archivos locales para que funcione

--- admin/config/templates_c/%%0E^0E4^0E407559%%footer.tpl.php
+++ admin/config/templates_c/%%0E^0E4^0E407559%%footer.tpl.php
@@ -1,4 +1,12 @@
-<?php /* Smarty version 2.6.15, created on 2007-01-05 14:46:50
+<?php /* Smarty version 2.6.15, created on 2007-01-09 17:58:36
          compiled from footer.tpl */ ?>
-</BODY>
-</HTML>
+</div><!-- Cierra bloque de proceso y termina la pagina -->
+<div class="footer">
+	<div class="footer-text">		
+		<span class="important">SIMERS S.A.</span><br />
+		<span>Todos los derechos estan reservados a los autores - 2007</span><br />
+		<span>Bogota D.C, Colombia</span>
+	</div>
+</div>
+</body>
+</html>
--- admin/config/templates_c/%%91^914^914D6B5F%%selectfile.tpl.php
+++ admin/config/templates_c/%%91^914^914D6B5F%%selectfile.tpl.php
@@ -1,8 +1,5 @@
-<?php /* Smarty version 2.6.15, created on 2007-01-05 13:07:42
+<?php /* Smarty version 2.6.15, created on 2007-01-09 18:03:35
          compiled from selectfile.tpl */ ?>
-<h1>El path del programa completo es: <?php echo $this->_tpl_vars['path']; ?>
-</h1>
-<br>
 <form action="loadfile.php" method="post" enctype="multipart/form-data">    
     <br>
     <b>Enviar un nuevo archivo: </b>
